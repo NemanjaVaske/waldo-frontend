90ca1c91682fe04b0eb787938cbd57384843f14f
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-09 15:55:49 +0100

    add id to characters

--- src/tagFindCharacter.js
+++ src/tagFindCharacter.js
@@ -1,10 +1,10 @@
-const tagFindCharacter = (x, y) => {
+const tagFindCharacter = (pos_x, pos_y) => {
   const mainImage = document.getElementsByClassName("picture-cover")[0];
   const tag = document.createElement("div");
   console.log(mainImage);
   tag.classList.add("tag");
-  tag.style.top = y + 6 + "px";
-  tag.style.left = x - 10 + "px";
+  tag.style.top = pos_y + 6 + "px";
+  tag.style.left = pos_x - 10 + "px";
   mainImage.appendChild(tag);
 };
 module.exports = tagFindCharacter;
