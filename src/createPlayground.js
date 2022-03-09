3c6bab690f500c31913389e6c6e25b56bf287695
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-09 12:52:12 +0100

    add playground

---
+++ src/createPlayground.js
@@ -0,0 +1,23 @@
+const createPlayground = (pictureUrl) => {
+  const content = document.getElementById("content");
+  const characters = document.createElement("div");
+  characters.classList.add("characters");
+  const waldo = document.createElement("img");
+  waldo.setAttribute("src", "./assets/image/waldo.jpg");
+  characters.appendChild(waldo);
+  const wenda = document.createElement("img");
+  wenda.setAttribute("src", "./assets/image/wenda.jpg");
+  characters.appendChild(wenda);
+  const wizzard = document.createElement("img");
+  wizzard.setAttribute("src", "./assets/image/wizard.jpg");
+  characters.appendChild(wizzard);
+  const odlaw = document.createElement("img");
+  odlaw.setAttribute("src", "./assets/image/odlaw.jpg");
+  characters.appendChild(odlaw);
+  content.appendChild(characters);
+  const mainPic = document.createElement("img");
+  mainPic.setAttribute("src", `./assets/image/${pictureUrl}`);
+  mainPic.classList.add("main-picture");
+  content.appendChild(mainPic);
+};
+module.exports = createPlayground;
--- src/letsPlay.js
+++ src/letsPlay.js
@@ -1,10 +1,12 @@
 const clear = require("./clear");
+const createPlayground = require("./createPlayground");
 const letsPlay = async (imageId) => {
   if (imageId) {
     const res = await fetch(`http://localhost:3000/api/v1/images/${imageId}`);
     const data = await res.json();
     //removing all from content
     clear();
+    createPlayground(data.url);
   }
 };
 module.exports = letsPlay;
