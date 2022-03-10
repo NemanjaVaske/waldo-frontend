fc880788012b00f1230e86ab588acbd15e323675
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-10 13:46:54 +0100

    changed about displaying right time

--- src/sendForm.js
+++ src/sendForm.js
@@ -1,16 +1,21 @@
 const sendForm = async (name, record, image) => {
   let records = 2;
+  records = record.split(":");
+  let min = parseInt(records[0], 10);
+  let sec = parseInt(records[1], 10);
+  records = sec + min * 60;
   const res = await fetch(
     `http://localhost:3000/api/v1/images/${image.id}/scores/${name}/${records}`,
     { method: "post" }
   );
   const data = await res.json();
-  console.log(data);
   const content = document.getElementById("content");
   const list = document.createElement("ol");
   data.forEach((char) => {
     const listItem = document.createElement("li");
-    listItem.innerHTML = `${char.name} with score: ${data.seconds}`;
+    let sec = char.seconds % 60;
+    let min = char.seconds / 60;
+    listItem.innerHTML = char.name + " with score " + min.toFixed() + ":" + sec;
     list.appendChild(listItem);
   });
   content.appendChild(list);
